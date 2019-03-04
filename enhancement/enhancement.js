f87bdedc5628ff498854ca5d610936f0f545f37c
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-03-04 15:45:22 -0600

    Added a test case for a enhancement success which increases the enchancement level by 1 and changes the display name of the item in accordance to the new enchancement level

--- enhancement/enhancement.js
+++ enhancement/enhancement.js
@@ -1,6 +1,30 @@
 module.exports = {
-    repair
+    repair,
+    success
 };
+
+function success(item) {
+    const newItem = {...item};
+
+    newItem.enchancment += 1;
+    
+    if(newItem.enchancment === 16){
+        newItem.displayName = `[PRI] ${newItem.name}`;
+    } else if(newItem.enchancment === 17){
+        newItem.displayName = `[DUO] ${newItem.name}`;
+    } else if(newItem.enchancment === 18){
+        newItem.displayName = `[TRI] ${newItem.name}`;
+    }else if(newItem.enchancment === 19){
+        newItem.displayName = `[TET] ${newItem.name}`;
+    }else if(newItem.enchancment === 20){
+        newItem.displayName = `[PEN] ${newItem.name}`;
+    }else {
+        newItem.displayName = `[+${newItem.enchancment}] ${newItem.name}`;
+    }
+    
+
+    return newItem;
+}
 
 function repair(item) {
     const newItem = {...item};
--- enhancement/enhancement.spec.js
+++ enhancement/enhancement.spec.js
@@ -1,13 +1,41 @@
-const { repair } = require('./enhancement.js');
+const { repair, success } = require('./enhancement.js');
 
-it('should repair item to 100 durability', () => {
-    const item = {
-        name: 'Rosar shortsword',
-        type: 'Weapon',
-        durability: 50,
-        enhancement: 1,
-        displayName: `[+${this.enhancement}] ${this.name}`
-    };
+describe('repair()', () => {
+    it('should repair item to 100 durability', () => {
+        const item = {
+            name: 'Rosar Shortsword',
+            type: 'Weapon',
+            durability: 50,
+            enhancement: 1,
+            displayName: `[+1] Rosar Shortsword`
+        };
+    
+        expect(repair(item).durability).toBe(100);
+    });
+});
 
-    expect(repair(item).durability).toBe(100);
-})
+describe('success()', () => {
+    it('should increase the enchancment level by 1', () => {
+        const item = {
+            name: 'Kzarkas Shortsword',
+            type: 'Weapon',
+            durability: 20,
+            enchancment: 3,
+            displayName: `[+3] Kzarkas Shortsword`
+        };
+    
+        expect(success(item).enchancment).toBe(item.enchancment+1);
+    });
+    
+    it('should change the display name to reflect the new enchancment level', () => {
+        const item = {
+            name: 'Kzarkas Shortsword',
+            type: 'Weapon',
+            durability: 20,
+            enchancment: 15,
+            displayName: `[+15] Kzarkas Shortsword`
+        };
+
+        expect(success(item).displayName).toBe('[PRI] Kzarkas Shortsword');
+    });
+});
